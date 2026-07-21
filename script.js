57054fb837f12c925dcce1f1eb0a852f2d14b54b
Jessmach77 <infocreationhomemade@gmail.com>
2026-07-21 10:55:51 +0200

    Refactor supabase integration and add type check alert
    Removed supabase client initialization and event listener for form submission. Added alert to check supabase type.

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
+alert("Script partito");
+
 const supabaseUrl = "https://bazubrbkwylvjgqqdurz.supabase.co";
 const supabaseKey = "sb_publishable_b8oWkbN-XRsADQ80ni10xw_Sje4LGhE";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-
-document.getElementById("inizia").addEventListener("click", async () => {
-
-    const nome = document.getElementById("nome").value.trim();
-    const email = document.getElementById("email").value.trim();
-
-    const { data, error } = await supabase
-        .from("partecipanti")
-        .insert([
-            {
-                nome: nome,
-                email: email
-            }
-        ]);
-
-    if (error) {
-        alert(error.message);
-        console.log(error);
-        return;
-    }
-
-    alert("Salvato con successo!");
-
-});
+alert(typeof window.supabase);
